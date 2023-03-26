1df5489aa96be9859f48160c3e7dcff199521c6c
Craig <indigomx9@gmail.com>
2023-03-25 21:33:15 -0400

    Initial setup of the project.

---
+++ src/@types/custom.d.ts
@@ -0,0 +1,12 @@
+declare module "*.gif";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.webp";
+declare module "*.avif";
+declare module "*.gif";
+
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus quae quos recusandae magni similique delectus. Consequatur beatae nam ullam error, cupiditate praesentium eius culpa repudiandae modi magni blanditiis aliquid voluptatibus?</p>
+        </React.Fragment>
+    );
+};
+
+
